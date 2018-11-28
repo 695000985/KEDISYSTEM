72ab3391918c332ba91ba9c3d6ac8b5638e1fbc3
yt <812564081@qq.com>
2018-11-28 10:58:48 +0800

    download

--- script/common.js
+++ script/common.js
@@ -360,19 +360,70 @@ function get_version(fn) { // 获取软件的版本号
     })
 }
 
+//导出表格
+function JSONToExcelConvertor(JSONData, FileName, ShowLabel) {
+ 			
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    console.log(arrData);
+    var excel = '<table>';
+     
+    //设置表头
+    var row = "<tr>";
+    for (var i = 0, l = ShowLabel.length; i < l; i++) {
+             row += "<td>" + ShowLabel[i].value + '</td>';
+    }   
+    //换行
+    excel += row + "</tr>";
+     
+    //设置数据
+    for (var i = 0; i < 800; i++) {
+         var row = "<tr>";
+     
+       for(var j=0;j<arrData[i].length;j++){
+         var value = arrData[i][j].value === "." ? "" : arrData[i][j].value;
+         row += '<td>' + value + '</td>';
+      }
+    
+       excel += row + "</tr>";
+    }
+     
+    excel += "</table>";
+     
+    var excelFile = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:x='urn:schemas-microsoft-com:office:excel' xmlns='http://www.w3.org/TR/REC-html40'>";
+    excelFile += '<meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8">';
+    excelFile += '<meta http-equiv="content-type" content="application/vnd.ms-excel';
+    excelFile += '; charset=UTF-8">';
+    excelFile += "<head>";
+    excelFile += "<!--[if gte mso 9]>";
+    excelFile += "<xml>";
+    excelFile += "<x:ExcelWorkbook>";
+    excelFile += "<x:ExcelWorksheets>";
+    excelFile += "<x:ExcelWorksheet>";
+    excelFile += "<x:Name>";
+    excelFile += "{worksheet}";
+    excelFile += "</x:Name>";
+    excelFile += "<x:WorksheetOptions>";
+    excelFile += "<x:DisplayGridlines/>";
+    excelFile += "</x:WorksheetOptions>";
+    excelFile += "</x:ExcelWorksheet>";
+    excelFile += "</x:ExcelWorksheets>";
+    excelFile += "</x:ExcelWorkbook>";
+    excelFile += "</xml>";
+    excelFile += "<![endif]-->";
+    excelFile += "</head>";
+    excelFile += "<body>";
+    excelFile += excel;
+    excelFile += "</body>";
+    excelFile += "</html>";
 
-
-
-function newApi(format,form_name) { //导出数据
-    return ExcellentExport.convert({
-        anchor: 'anchorNewApi-' + format,
-        filename: 'TellerSearch',
-        format: format
-    }, [{
-        name: 'Sheet Name Here 1',
-        from: {
-            table: form_name
-        }
-    }]);
-
+    var uri = 'data:application/vnd.ms-excel;charset=utf-8,' + encodeURIComponent(excelFile);    
+    var link = document.createElement("a");
+    link.href = uri;
+    
+    link.style = "visibility:hidden";
+    link.download = FileName + ".xls";
+    
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
