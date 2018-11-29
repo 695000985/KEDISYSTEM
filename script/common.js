13f7656ea3b059380bdfe55ed28c334cd57621e8
yt <812564081@qq.com>
2018-11-29 19:17:33 +0800

    debt

--- script/common.js
+++ script/common.js
@@ -427,3 +427,25 @@ function JSONToExcelConvertor(JSONData, FileName, ShowLabel) {
     link.click();
     document.body.removeChild(link);
 }
+
+
+
+$('#index_content dl dd ul li a').on('click',function () {
+    console.log('5555')
+    // console.log($(this).attr('data-title'))
+})
+
+function GetRequest(value) {  
+    var url = decodeURI(location.search); //?id="123456"&Name="bicycle";
+    var object = {};
+    if(url.indexOf("?") != -1)//url中存在问号，也就说有参数。  
+    {   
+      var str = url.substr(1);  //得到?后面的字符串
+      var strs = str.split("&");  //将得到的参数分隔成数组[id="123456",Name="bicycle"];
+      for(var i = 0; i < strs.length; i ++)  
+        {   
+　　　　　　　　object[strs[i].split("=")[0]]=strs[i].split("=")[1]
+　　　　　　}
+　　}
+    return object[value];  
+}  
