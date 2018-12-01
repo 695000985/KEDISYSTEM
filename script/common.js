2f77457ad9e529d3431c475f3065d43a0a66cc26
yt <812564081@qq.com>
2018-12-01 15:11:24 +0800

    m_modal

--- script/common.js
+++ script/common.js
@@ -454,7 +454,7 @@ function GetRequest(value) {
 
 function showErr_bs(msg){
     console.log('adadad')
-    var alert = '<div class="alert alert-warning alert-dismissible" role="alert" style="padding: 25px 15px;color:#333333;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);box-shadow:0px 0px 3px 2px #cccccc;background-color:#ffffff;border-color:#ffffff;">'+
+    var alert = '<div class="alert alert-warning alert-dismissible" role="alert" style="z-index:10000;padding: 25px 15px;color:#333333;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);box-shadow:0px 0px 3px 2px #cccccc;background-color:#ffffff;border-color:#ffffff;">'+
         '<button type="button" class="close_ close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true"></span></button>'+
         '<strong></strong>'+msg+
         '</div>'
